afb293de2c86f8e447d52a651974251a5c83faf4
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-08 08:39:11 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1/atividade_01/atividade_01/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int number;
+        string positiveOrNegative;
+
+        Console.WriteLine("Informe um número:");
+        number = int.Parse(Console.ReadLine());
+        Console.Clear();
+
+        positiveOrNegative = PositiveOrNegative(number);
+        Console.WriteLine(positiveOrNegative);
+    }
+
+    static string PositiveOrNegative(int number)
+    {
+        if (number < 0) return $"{number} é um número Negativo";
+
+        else if (number == 0) return $"O número é igual a {number}";
+
+        else return $"{number} é um número Positivo";
+    }
+}
