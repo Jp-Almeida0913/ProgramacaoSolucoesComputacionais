ee2ab667283bbce5bb3ae6e7b6c2a62699bc72cb
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-08 08:59:19 -0300

    Adiciona projeto atividade_02 com funcionalidade básica
    Criação do arquivo de solução `atividade_02.sln` com configurações para Debug e Release. Implementação da classe `Program` em `Program.cs`, que solicita um número ao usuário e determina se é par ou ímpar. Atualização do arquivo de projeto `atividade_02.csproj` para usar o SDK do .NET 8.0, definindo o tipo de saída como executável e habilitando usabilidade implícita e nullable.

---
+++ Lista1/atividade_02/atividade_02/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int number;
+        String evenOrOdd;
+
+        Console.WriteLine("Informe um número:");
+        number = int.Parse(Console.ReadLine());
+
+        evenOrOdd = EvenOrOdd(number);
+        Console.WriteLine(evenOrOdd);
+    }
+
+    static String EvenOrOdd(int number)
+    {
+        if (number % 2 == 0) return "Par";
+
+        else return "Impar";
+    }
+}
