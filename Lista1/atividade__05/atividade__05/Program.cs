43dd483eb888b7301140f57576049af865640917
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-09 09:58:13 -0300

    Adicionar projeto .NET 8.0 com funcionalidade básica
    Criação e configuração do arquivo de solução `atividade__05.sln` para Visual Studio 17, incluindo definições de compilação Debug e Release.
    
    Adição do arquivo `Program.cs` com uma classe `Program` que solicita dois números ao usuário, calcula o menor e exibe o resultado.
    
    Atualização do arquivo `atividade__05.csproj` para definir o projeto como um aplicativo executável utilizando o framework .NET 8.0, com usings implícitos e suporte a tipos nulos.

---
+++ Lista1/atividade__05/atividade__05/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double x, y, minOf;
+
+        Console.Write("Informe um número --> ");
+        x = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe outro número --> ");
+        y = double.Parse(Console.ReadLine());
+
+        minOf = Math.Min(x, y);
+        Console.WriteLine(minOf);
+    }
+}
