5967b8e847f026823be3e7144abd9d68ffea8882
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-09 09:54:36 -0300

    Adicionar projeto atividade_04 com funcionalidade básica
    Criação e configuração do arquivo de solução `atividade_04.sln` com suporte para Visual Studio e definição do projeto.
    
    Adição do arquivo `Program.cs` com uma classe `Program` que solicita dois números ao usuário, calcula o máximo entre eles e exibe o resultado.
    
    Atualização do arquivo de projeto `atividade_04.csproj` para definir o tipo de saída como executável, especificar o framework .NET 8.0 e habilitar usings implícitos e nullable.

---
+++ Lista1/atividade_04/atividade_04/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double x, y, max;
+
+        Console.Write("Informe um número --> ");
+        x = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe outro número --> ");
+        y = double.Parse(Console.ReadLine());
+
+        max = Math.Max(x, y);
+        Console.WriteLine(max);
+    }
+}
