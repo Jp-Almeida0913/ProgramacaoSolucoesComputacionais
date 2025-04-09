e60aee8606858c750f0d276ad4ec3216ce73853a
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-09 09:40:58 -0300

    Adiciona projeto de cálculo de volume de esfera
    Criação e configuração do arquivo `atividade_03.sln` para o projeto, incluindo plataformas de configuração e propriedades do Visual Studio.
    
    Adição do arquivo `Program.cs` com a classe `Program`, que solicita o raio da esfera e calcula seu volume, exibindo o resultado no console.
    
    Modificação do arquivo `atividade_03.csproj` para definir o tipo de saída como executável e o framework alvo como .NET 8.0, além de habilitar usings implícitos e nullable.

---
+++ Lista1/atividade_03/atividade_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double radius, volume;
+
+        Console.WriteLine("Insira o Raio da esfera para saber o seu volume:");
+        radius = double.Parse(Console.ReadLine());
+
+        volume = CalculateSphereVolume(radius);
+        Console.WriteLine($"A esfera de raio {radius} possuí o volume = {volume}");
+    }
+
+    static double CalculateSphereVolume(double radius)
+    {
+        const double pi = 3.14;
+        double result;
+
+        result = (4.0 / 3.0) * (pi * Math.Pow(radius, 3.0));
+        return result;
+    }
+}
