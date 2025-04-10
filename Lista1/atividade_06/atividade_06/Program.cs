79387cd243d56378fa1babd98d1eccd3f4821425
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-10 09:59:42 -0300

    Adicionar projeto atividade_06 com lógica de comparação
    Criado ou atualizado o arquivo de solução `atividade_06.sln` com informações do Visual Studio e configuração do projeto. Adicionado o arquivo `Program.cs` com uma classe `Program` que solicita três números ao usuário e exibe o maior deles. Atualizado o arquivo de projeto `atividade_06.csproj` para usar o SDK do .NET 8.0, definindo o tipo de saída como executável e habilitando usings implícitos e nullable.

---
+++ Lista1/atividade_06/atividade_06/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double num1, num2, num3, maxOf, higherNumber;
+
+        Console.Write("Informe o primeiro número --> ");
+        num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o segundo número --> ");
+        num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o terceiro número --> ");
+        num3 = double.Parse(Console.ReadLine());
+
+        higherNumber = Math.Max(num1, num2);
+        maxOf = Math.Max(higherNumber, num3);
+
+        Console.WriteLine(maxOf);
+    }
+}
