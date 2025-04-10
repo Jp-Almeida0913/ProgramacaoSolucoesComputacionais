dd8db135453781ade01a3a302f46af7cd115a383
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-10 10:21:35 -0300

    Adicionar projeto atividade_07 com lógica de números
    Criado ou atualizado o arquivo de solução `atividade_07.sln` com informações de versão do Visual Studio e configurações de compilação para Debug e Release.
    
    Adicionado o arquivo `Program.cs` com uma classe `Program` que solicita três números ao usuário e calcula o menor entre eles.
    
    Atualizado o arquivo de projeto `atividade_07.csproj` para usar o SDK do .NET, definindo o tipo de saída como executável e o framework alvo como .NET 8.0, além de habilitar o uso implícito de namespaces e a funcionalidade de tipos nulos.

---
+++ Lista1/atividade_07/atividade_07/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double number1, number2, number3, minOf, lowerNumber;
+
+        Console.Write("Informe o primeiro número --> ");
+        number1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o segundo número --> ");
+        number2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o terceiro número --> ");
+        number3 = double.Parse(Console.ReadLine());
+
+        lowerNumber = Math.Min(number1, number2);
+        minOf = Math.Min(lowerNumber, number3);
+
+        Console.WriteLine(minOf);
+    }
+}
