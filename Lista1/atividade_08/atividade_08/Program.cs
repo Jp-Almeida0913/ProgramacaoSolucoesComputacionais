36ae3f4884596f2b2fddd4f9ed3bdeb0ea9c939c
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-10 11:08:30 -0300

    Adicionar projeto de conversão de temperatura
    Criado ou atualizado o arquivo de solução `atividade_08.sln` com informações de configuração do projeto.
    Adicionado o arquivo `Program.cs` que implementa um programa para conversão de temperaturas entre Celsius e Fahrenheit.
    Atualizado o arquivo de projeto `atividade_08.csproj` para definir o tipo de saída como executável e especificar o framework .NET 8.0, habilitando usings implícitos e suporte a tipos nulos.

---
+++ Lista1/atividade_08/atividade_08/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int option;
+        double temperature, conversion;
+
+        Console.Write("Escolha qual conversão deseja realizar:\n" +
+            "1) De Celsius para Fahrenheit\n" +
+            "2) De Fahrenheit para Celsius\n" +
+            "--> ");
+
+        option = int.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                Console.Write("Informe o valor em Celsius que deseja converter para Fahrenheit:\n--> ");
+                temperature = double.Parse(Console.ReadLine());
+
+                conversion = CelsiusToFahrenheit(temperature);
+                Console.WriteLine($"Temperatura convertida: {conversion:F2} °F");
+                break;
+
+            case 2:
+                Console.Write("Informe o valor em Fahrenheit que deseja converter para Celsius:\n--> ");
+                temperature = double.Parse(Console.ReadLine());
+
+                conversion = FahrenheitToCelsius(temperature);
+                Console.WriteLine($"Temperatura convertida: {conversion:F2} °C");
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
+    }
+
+    static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * (9.0 / 5.0)) + 32;
+    }
+
+    static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * (5.0 / 9.0);
+    }
+}
