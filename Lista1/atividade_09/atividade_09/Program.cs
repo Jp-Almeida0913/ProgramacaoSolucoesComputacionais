bcc56f818aa6d249ce1d4f366f18120cc22d773a
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-15 08:06:23 -0300

    Adicionar projeto de cálculo de médias de notas
    Criação do arquivo de solução `atividade_09.sln` com a configuração do projeto para o Visual Studio.
    Adição do arquivo `Program.cs`, que implementa a lógica para solicitar três notas ao usuário, calcular a média, a média das duas maiores notas e identificar a maior e a menor nota, utilizando a classe `GradeAverageCalculator`.
    Criação do arquivo de projeto `atividade_09.csproj`, configurando o uso do SDK do .NET, definindo o tipo de saída como executável e o framework alvo como `net8.0`, além de habilitar usings implícitos e a verificação de nulidade.

---
+++ Lista1/atividade_09/atividade_09/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        double num1, num2, num3, average, topAverage, highestGrade, lowestGrade;
+
+        Console.Write("Insira a primeira nota --> ");
+        num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Insira a segunda nota --> ");
+        num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Insira a terceira nota --> ");
+        num3 = double.Parse(Console.ReadLine());
+
+        Console.Clear();
+
+        GradeAverageCalculator studentGrade = new GradeAverageCalculator(num1, num2, num3);
+
+        average = studentGrade.GradeAverage();
+        Console.WriteLine($"Média das notas: {average}");
+
+        topAverage = studentGrade.AverageOfTopGrades();
+        Console.WriteLine($"Média das duas maiores notas: {topAverage}");
+
+        studentGrade.GetTopAndLowestGrades(out highestGrade, out _, out lowestGrade);
+        Console.WriteLine($"Maior nota: {highestGrade}, Menor nota: {lowestGrade}");
+    }
+
+    class GradeAverageCalculator
+    {
+        public double number1;
+        public double number2;
+        public double number3;
+        public List<double> grades;
+
+        public GradeAverageCalculator(double num1, double num2, double num3)
+        {
+            number1 = num1;
+            number2 = num2;
+            number3 = num3;
+            grades = new List<double> { number1, number2, number3 };
+        }
+        public double GradeAverage()
+        {
+            return grades.Average();
+        }
+
+        public void GetTopAndLowestGrades(out double highestGrade, out double secondHighestGrade, out double lowestGrade)
+        {
+            grades.Sort();
+            highestGrade = grades[2];
+            secondHighestGrade = grades[1];
+            lowestGrade = grades[0];
+        }
+
+        public double AverageOfTopGrades()
+        {
+            GetTopAndLowestGrades(out double highestGrade, out double secondHighestGrade, out _);
+            return (highestGrade + secondHighestGrade) / 2;
+        }
+    }
+}
