1ad02640fd969a2db7e9e60d2a3c432fdf3e3fe8
Jp-Almeida0913 <joaoalmeida0913@outlook.com>
2025-04-15 22:43:46 -0300

    Adicionar implementação de equação quadrática
    Adicionado o arquivo `Program.cs` com a classe `Program` que solicita os valores de A, B e C e calcula as raízes da equação quadrática através da classe `QuadraticEquation`. O método `ToString` foi sobrescrito para exibir a equação e os resultados formatados.
    
    Criado o arquivo `atividade_10.csproj` configurando um projeto executável com .NET 8.0, habilitando usings implícitos e nullable.

---
+++ Lista1/atividade_10/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double a, b, c;
+
+        Console.Write("Insira o valor de A --> ");
+        a = double.Parse(Console.ReadLine());
+
+        Console.Write("Insira o valor de B --> ");
+        b = double.Parse(Console.ReadLine());
+
+        Console.Write("Insira o valor de C --> ");
+        c = double.Parse(Console.ReadLine());
+
+        QuadraticEquation test = new QuadraticEquation(a, b, c);
+        Console.Write(test);
+
+    }
+}
+
+public class QuadraticEquation
+{
+    double A;
+    double B;
+    double C;
+
+    public QuadraticEquation(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public double Delta()
+    {
+        return Math.Pow(B, 2.0) - 4 * (A * C);
+    }
+
+    public double FirstBhaskara(double delta)
+    {
+        delta = Delta();
+        return (-B + Math.Sqrt(delta)) / 2;
+    }
+
+    public double SecondBhaskara(double delta)
+    {
+        delta = Delta();
+        return (-B - Math.Sqrt(Delta())) / 2;
+    }
+
+    public override string ToString()
+    {
+        string bFormatted = B >= 0 ? $"+ {B}" : $"- {Math.Abs(B)}";
+        string cFormatted = C >= 0 ? $"+ {C}" : $"- {Math.Abs(C)}";
+
+        return $"A equação de segundo grau:\n\n{A}x² {bFormatted}x {cFormatted} = 0\n\nTem como resultado: {FirstBhaskara} e {SecondBhaskara}";
+    }
+}
